5331e8aca14316f5a3db7cef55dd7082ffce55b0
Fernando Romo <pop@cofradia.org>
2022-09-07 11:15:30 -0500

    sequence and totals for the ga_cli_to_qr.pl program

--- ga_cli_to_qr.pl
+++ ga_cli_to_qr.pl
@@ -85,7 +85,7 @@ foreach my $issuer (sort { "\U$a" cmp "\U$b" } keys %key_ring) {
                 darkcolor     => Imager::Color->new(0, 0, 0),
          );
          my $img = $qrcode->plot("$ga_qr$mime_data");
-         my $qr_file = 'bulk_keys_' . sprintf("%02d",$current) . '.jpg';
+         my $qr_file = 'bulk_keys_' . sprintf("%02d",$current) . '_of_' . sprintf("%02d", $images_count) . '.jpg';
          $img->write(file => "$qr_file");
          $bulk_ring{'Index'} = ();
          $bulk_ring{current} = $current++;
